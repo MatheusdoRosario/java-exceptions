37508e5e77005eecf94b21bfaebcef1a484b37fd
MatheusDoRosario <matheusxdargws@gmail.com>
2026-02-12 10:51:15 -0300

    Adiciona tratamento de excecao na AdocaoController

--- src/main/java/adopet/api/service/AdocaoService.java
+++ src/main/java/adopet/api/service/AdocaoService.java
@@ -44,12 +44,8 @@ public class AdocaoService {
 
     public void aprovar(AprovarAdocaoDTO dto){
         Adocao adocao = adocaoRepository.getReferenceById(dto.idAdocao());
-        try {
             adocao.marcarComoAprovada();
             adocao.getPet().marcarComoAdotado();
-        } catch (EntityNotFoundException ex) {
-            System.out.println("Pet não encontrado!");
-        }
     }
 
     public void reprovar(ReprovarAdocaoDTO dto){
